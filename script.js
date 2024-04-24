1c50b2c74b01174ce2e9a0a701c63be98409b576
Dipto Neogi <81651700+Dipto07@users.noreply.github.com>
2024-04-24 06:58:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
 const makeChange = (c) => {
-    const quarterValue = 0.25;
-    const dimeValue = 0.10;
-    const nickelValue = 0.05;
-    const pennyValue = 0.01;
+   const quarterValue = 25;
+    const dimeValue = 10;
+    const nickelValue = 5;
+    const pennyValue = 1;
 
     c = parseFloat(c); 
 
-    const quarters = Math.floor(c / quarterValue);
+    let quarters = Math.floor(c / quarterValue);
     c %= quarterValue;
 
-    const dimes = Math.floor(c / dimeValue);
+    let dimes = Math.floor(c / dimeValue);
     c %= dimeValue;
 
-    const nickels = Math.floor(c / nickelValue);
+    let nickels = Math.floor(c / nickelValue);
     c %= nickelValue;
 
-    const pennies = Math.round(c / pennyValue);
+    let pennies = Math.floor(c / pennyValue);
 
     return { q: quarters, d: dimes, n: nickels, p: pennies };
 };
-
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
 
